16dcccbefc90bd918231ce4ba2999b1fc6ce05d8
Kengo Shibata <cute_poison@me.com>
2020-06-10 20:06:38 +0900

    changed app text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Bye bye.</p>
+        <p>I was changed on the Feature branch.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
